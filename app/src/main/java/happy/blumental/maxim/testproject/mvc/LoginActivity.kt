a428bc45ec19e911518a3d453e944253a3bf486f
Maxim Blumental <bvmaks@gmail.com>
2015-10-21 20:06:16 +0300

    Signup/login corner cases handling. Some other minor changes.

--- app/src/main/java/happy/blumental/maxim/testproject/mvc/LoginActivity.kt
+++ app/src/main/java/happy/blumental/maxim/testproject/mvc/LoginActivity.kt
@@ -1,9 +1,14 @@
 package happy.blumental.maxim.testproject.mvc
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseException
+import com.parse.ParseUser
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import happy.blumental.maxim.testproject.MainActivity
 import happy.blumental.maxim.testproject.R
 
 class LoginActivity : RxAppCompatActivity() {
@@ -15,15 +20,80 @@ class LoginActivity : RxAppCompatActivity() {
         val loginEditText = findViewById(R.id.loginEditText) as EditText
         val passwordEditText = findViewById(R.id.passwordEditText) as EditText
         val logInButton = findViewById(R.id.logInButton)
+        val signUpButton = findViewById(R.id.signUpButton)
 
         logInButton.setOnClickListener {
             view ->
-            val login = loginEditText.text.toString()
-            val pswd = passwordEditText.text.toString()
-
-            if ( login.equals("") || pswd.equals(""))
-                showMissingCredentialsAlert()
+            logIn(loginEditText.text.toString(),
+                    passwordEditText.text.toString())
         }
+
+        signUpButton.setOnClickListener {
+            view ->
+            signUp(loginEditText.text.toString(),
+                    passwordEditText.text.toString())
+        }
+    }
+
+    private fun signUp(login: String, pswd: String) {
+        val user = ParseUser();
+        user.setUsername(login);
+        user.setPassword(pswd);
+
+        user.signUpInBackground(
+                { e ->
+                    if (e == null) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // Sign up didn't succeed. Look at the ParseException
+                        // to figure out what went wrong
+                        when (e.getCode()) {
+                            ParseException.USERNAME_TAKEN -> showNameTakenAlert()
+                            else -> {
+                                Toast.makeText(this, "Failed with exception ${e.getCode()}.",
+                                        Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                })
+    }
+
+    private fun logIn(login: String, pswd: String) {
+
+        if ( login.equals("") || pswd.equals("")) {
+            showMissingCredentialsAlert()
+            return
+        }
+
+        //check if there's a user with such credentials
+        ParseUser.logInInBackground(login, pswd,
+                {
+                    user, e ->
+                    if (user != null) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // Signup failed. Look at the ParseException to see what happened.
+                        when (e.getCode()) {
+                            ParseException.OBJECT_NOT_FOUND -> showNoSuchUserAlert()
+                            else -> Toast.makeText(this, "Failed with exception ${e.getCode()}.",
+                                    Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+    }
+
+    private fun showNoSuchUserAlert() {
+        val alertDialog = AlertDialog.Builder(this)
+
+        alertDialog.setTitle("No such user")
+                .setMessage("You either mistyped the credentials or should sign up first.")
+                .setCancelable(true)
+                .setNegativeButton("OK", { dialog, which -> dialog.cancel() })
+                .setOnCancelListener { dialog -> dialog.cancel() }
+
+        alertDialog.create().show()
     }
 
     private fun showMissingCredentialsAlert() {
@@ -31,6 +101,18 @@ class LoginActivity : RxAppCompatActivity() {
 
         alertDialog.setTitle("You forgot something")
                 .setMessage("Either login or password is missing.")
+                .setCancelable(true)
+                .setNegativeButton("OK", { dialog, which -> dialog.cancel() })
+                .setOnCancelListener { dialog -> dialog.cancel() }
+
+        alertDialog.create().show()
+    }
+
+    private fun showNameTakenAlert() {
+        val alertDialog = AlertDialog.Builder(this)
+
+        alertDialog.setTitle("The login is taken")
+                .setMessage("Please, select another login.")
                 .setCancelable(true)
                 .setNegativeButton("OK", { dialog, which -> dialog.cancel() })
                 .setOnCancelListener { dialog -> dialog.cancel() }
